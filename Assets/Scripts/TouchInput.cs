4a4fb769df15cfb3b9cd44c49ec801b73b26fa9c
Dani John <rocksdanister@gmail.com>
2018-09-29 02:09:35 +0530

    Update TouchInput.cs

--- Assets/Scripts/TouchInput.cs
+++ Assets/Scripts/TouchInput.cs
@@ -16,6 +16,7 @@ public class TouchInput : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+	 // Not using it, decided to only use accelerometer input.
         /*
         if (Input.touchCount > 0)
         {
